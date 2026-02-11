af14f65840b9ca09da4efa0e9928a8b18f3d12fa
Suhani Lodhi <suhanilodhi530@gmail.com>
2026-02-11 18:50:24 +0530

    fixed import issue

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,6 @@
 
 import React,{ useContext } from 'react';
-import './FoodDisplay.css'
+import './foodDisplay.css';
 import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
