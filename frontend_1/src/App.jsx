9b174a9c2509f59bb6c942de8202a9d8da2f2ba0
Suhani Lodhi <suhanilodhi530@gmail.com>
2026-04-20 13:58:04 +0530

    update App.jsx

--- frontend_1/src/App.jsx
+++ frontend_1/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import OrderTracking from "./pages/OrderTracking/OrderTracking";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/order-status/:id" element={<OrderTracking />} />
         </Routes>
       </div>
       <Footer />
